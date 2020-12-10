27a0e7af2c5bff29132acf00705f45d4815b5f7b
farzinnegahbani <farzin.negahbani@gmail.com>
2020-12-10 18:44:32 +0300

    Moving to object oriented structure, added batch annotation

--- annotate_p3.py
+++ annotate_p3.py
@@ -1,139 +1,262 @@
 import open3d as o3d
 import numpy as np
 import pickle as pkl
-import time
-from matplotlib import pyplot as plt
+import copy
+import logging as log
+
+
+
+def filter_mesh(mesh, iterations, visualize=False, verbose=False):
+        '''
+
+        '''
+        mesh_out = mesh.filter_smooth_simple(number_of_iterations=iterations)
+        mesh_out.compute_vertex_normals()
+        if visualize:
+            o3d.visualization.draw_geometries([mesh_out])
+
+        return mesh_out
+
+
+class Data:
+    '''
+        A helper class to keep information about a sample of data and annotations provided by the annotator class.
+    '''
+    def __init__(self, pcd_path):
+
+        self.pcd         = o3d.io.read_point_cloud(pcd_path)
+        self.arm_ind     = None
+        self.arm_pcd     = None
+        self.arm_rec_pcd = None
+
+    def surface_reconstruct_arm_pcd(self, visualize=False, verbose=False, sampling_method='poisson_disk', filter_it=0):
+        '''
+
+        '''
+
+        if verbose:
+            log.basicConfig(format="%(levelname)s: %(message)s", level=log.DEBUG)
+        else:
+            log.basicConfig(format="%(levelname)s: %(message)s")
+
+        log.info("Compute the normal of the point cloud.")
+
+        self.arm_pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.05, max_nn=10))
+
+        log.info('run Poisson surface reconstruction.')
+        
+        with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+            arm_mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(self.arm_pcd, depth=10)
+
+        if visualize:
+            log.info('visualizing mesh.')
+            o3d.visualization.draw_geometries([arm_mesh])
+
+        densities = np.asarray(densities)
+
+        if visualize:
+            from matplotlib import pyplot as plt
+            log.info('visualizing estimated densities.')
+            density_colors = plt.get_cmap('plasma')((densities - densities.min()) / (densities.max() - densities.min()))
+            density_colors = density_colors[:, :3]
+            density_mesh = o3d.geometry.TriangleMesh()
+            density_mesh.vertices = arm_mesh.vertices
+            density_mesh.triangles = arm_mesh.triangles
+            density_mesh.triangle_normals = arm_mesh.triangle_normals
+            density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
+            o3d.visualization.draw_geometries([density_mesh])
+
+        log.info('removing low density vertices.')
+        vertices_to_remove = densities < np.quantile(densities, 0.08)
+        arm_mesh.remove_vertices_by_mask(vertices_to_remove)
+
+        if visualize:
+            log.info('visualizing mesh after removing low dencity verticies.')
+            o3d.visualization.draw_geometries([arm_mesh])
+
+        arm_mesh.compute_vertex_normals()
+
+        if filter_it !=0 :
+            arm_mesh = filter_mesh(arm_mesh, filter_it)
+
+        if sampling_method == 'poisson_disk':
+            self.arm_rec_pcd = arm_mesh.sample_points_poisson_disk(number_of_points=200000)
+        elif sampling_method == 'uniform':
+            self.arm_rec_pcd = arm_mesh.sample_points_uniformly(number_of_points=200000)
+        else:
+            log.error("Undefined sampling method")
+            exit
+
+        if visualize:
+            log.info('visualizing the sampled point cloud.')
+            o3d.visualization.draw_geometries([arm_mesh])
+
+
+    def update_arm_pcl_from_ind(self):
+        '''
+
+        '''
+        self.arm_pcd = self.pcd.select_by_index(self.arm_ind)
+        
+        # Selecting background
+        #self.bg_pcd.select_by_index(arm_ind, invert=True)
+
+    def remove_outliers(self, mode="statistical"):
+        '''
+
+        '''
+        if self.arm_pcd != None:
+            log.error("This operation changes the main pcd file. You cannot perform this after annotation.")
+            exit
+
+        if mode is "statistical":
+            cl, ind   = self.pcd.remove_statistical_outlier(nb_neighbors=40,std_ratio=2.0)
+            
+
+        elif mode is "radious":
+            cl, ind   = self.pcd.remove_radius_outlier(nb_points=20, radius=0.01)
+            self.pcd  = self.pcd.select_by_index(ind)
+
+        else:
+            log.error("Undefined sampling method")
+            exit
+
+        self.pcd = self.pcd.select_by_index(ind)
+
+
+    def write_rec_arm_to_pcd(self, path="rec_arm_cloud.pcd"):
+        '''
+
+        '''
+        o3d.io.write_point_cloud(path, self.arm_rec_pcd , write_ascii=False, compressed=False, print_progress=True)
+    
+
+    def write_arm_to_pcd(self, path="arm_cloud.pcd"):
+        '''
+
+        '''
+        o3d.io.write_point_cloud(path, self.arm_pcd , write_ascii=False, compressed=False, print_progress=True)
+        
+    def write_dataset_element(self, path):
+        '''
+
+        '''
+        o3d.io.write_point_cloud(path, self.pcd , write_ascii=False, compressed=False, print_progress=True)
+
+        #NOT COMPLETED
+        # NEED TO WRITE THE INDICES
+
+        ## ONUR LOOK HERER PLZ
+
+
 
 class Annotator:
     '''
         All the operations of annotaion is going to be integrated
         to this class (e.g. background aggregation, background subtraction, reconstruction, ...)
     '''
-    def __init__(self, ):
-        self.metrics     = None
-        self.bg_samples  = None
+    def __init__(self, load_bg_from_file=None):
 
-    def set_bg(self, samples):
+        if load_bg_from_file is None:
+            log.warning("Don't forget to set the background pcl with set_background later.")
+        else:
+            self.bg_pcl = o3d.io.read_point_cloud(load_bg_from_file)
+
+    def set_background(self, bg_data):
         '''
-            set bg samples of the annotator
+            set background samples of the annotator
                 
             Note: some preprocessing steps on bg samples can be added here (e.g. adding aggregating frames)
         '''
-        self.bg_samples = samples
+        self.bg_samples = bg_data[0]
 
-    def o3d_classify(self, bg_cloud, target_cloud, metric='distance_changed', removal_th=0.02, clip_depth=True, max_depth=1):
+    def annotate_batch(self, target_batch, bg_pcl=None):
         '''
-            Classifies 3D points into arm and background points
         '''
-        dists = target_cloud.compute_point_cloud_distance(bg_cloud)
-        dists = np.asarray(dists)
-        mask  = dists > removal_th
+        if bg_pcl is None:
+            bg_pcl = self.bg_pcl
+
+        for data in target_batch:
+            data.arm_ind = self.distance_annotate(data.pcd, bg_pcl, removal_th=0.02, clip_depth=True, max_depth=1.0)
+            data.update_arm_pcl_from_ind()
+        
+        return target_batch
+
+
+    def annotate_single(self, target_data, bg_pcl=None):
+        '''
+        Args:
+            param1 (Data)  : The data sample to annotate. 
+        
+        '''
+        if bg_pcl is None:
+            bg_pcl = self.bg_pcl
+        
+        target_data.arm_ind = self.distance_annotate(target_data.pcd, bg_pcl, removal_th=0.02, clip_depth=True, max_depth=1.0)
+        target_data.update_arm_pcl_from_ind()
+
+        return target_data
+
+
+    def distance_annotate(self, target_cloud, bg_pcl, removal_th=0.02, clip_depth=True, max_depth=1):
+        '''
+        Classifies 3D points into arm and background points
+            
+        Args:
+            param1 (Pointcloud)      : The background Pointcloud.
+            param2 (Pointcloud)      : The target Pointcloud set that is being annotated.
+            param3 (string, optional): The target Pointcloud set that is being annotated.
+        
+        Returns:
+            nparray: returns array of integer containing indices of the robot arm points.
+        '''
+
+        dists     = np.asarray(target_cloud.compute_point_cloud_distance(bg_pcl))        
+        arm_mask  = dists > removal_th
 
         if clip_depth:
-            points = np.asarray(target_cloud.points)
-            mask   = np.logical_and(points[:,2] < max_depth ,  mask)
+            points    = np.asarray(target_cloud.points)
+            arm_mask  = np.logical_and(points[:,2] < max_depth ,  arm_mask)
         
-        ind      = np.where(mask == True)[0]
-
-        return ind
-
+        return  np.where(arm_mask == True)[0]
+        
 
 
-def surface_reconstruct(pcd, visualize=False):
+if __name__ == "__main__":
 
-    print("Compute the normal of the point cloud")
-    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.05, max_nn=10))
-    #pcd.orient_normals_consistent_tangent_plane(100)
+    # set some parameters
+    visualize       = False
+    remove_outliers = False
+    reconstruct     = False
 
-    print('run Poisson surface reconstruction')
-    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-        mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=10)
+    target_data = Data('./_gitignore/pcd_files/data_samples/data_30.pcd')
     
-    if visualize:
-        print('visualizing mesh.')
-        o3d.visualization.draw_geometries([mesh])
+    log.info("Loading and annotating data.")
 
-    print('visualize densities')
-    densities = np.asarray(densities)
-    density_colors = plt.get_cmap('plasma')((densities - densities.min()) / (densities.max() - densities.min()))
-    density_colors = density_colors[:, :3]
-    density_mesh = o3d.geometry.TriangleMesh()
-    density_mesh.vertices = mesh.vertices
-    density_mesh.triangles = mesh.triangles
-    density_mesh.triangle_normals = mesh.triangle_normals
-    density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
+    ann         = Annotator(load_bg_from_file='./_gitignore/pcd_files/bg_data/bg_1.pcd')
+    
+   
+    target_data = ann.annotate_single(target_data)
 
+
+    if remove_outliers:
+        target_data.remove_outliers(mode="statistical")
 
     if visualize:
-        print('visualizing estimated densities.')
-        o3d.visualization.draw_geometries([density_mesh])
+        log.info("visualizing the arm points.")
+        o3d.visualization.draw_geometries([target_data.arm_pcd])
 
-    print('remove low density vertices')
-    vertices_to_remove = densities < np.quantile(densities, 0.08)
-    mesh.remove_vertices_by_mask(vertices_to_remove)
+        if reconstruct:    
+            log.info("visualizing the reconstructed points.")
+            o3d.visualization.draw_geometries([bg_cloud])
+            
+    log.info("Saving the arm PCD.")
+    target_data.write_arm_to_pcd("arm_cloud.pcd")
 
+    if reconstruct:
 
-    if visualize:
-        print('visualizing mesh after removing low dencity verticies.')
-        o3d.visualization.draw_geometries([mesh])
-
-    mesh.compute_vertex_normals()
-    
-    pcd_new = mesh.sample_points_uniformly(number_of_points=200000)
-
-    if visualize:
-        print('visualizing the sampled point cloud.')
-        o3d.visualization.draw_geometries([mesh])
-
-    return pcd_new
-
-
-visualize       = False
-remove_outliers = False
-reconstruct     = True
-
-pcd_path  = './_gitignore/pcd_files/bg_data/bg_1.pcd'
-pcd_obj_p = o3d.io.read_point_cloud(pcd_path)
-
-pcd_path  = './_gitignore/pcd_files/data_samples/data_30.pcd'
-pcd_obj_c = o3d.io.read_point_cloud(pcd_path)
-
-ann       = Annotator()
-
-start = time.time()
-arm_ind  = ann.o3d_classify(pcd_obj_p ,pcd_obj_c)
-end = time.time()
-print("elapsed time: ", end - start)
-
-arm_cloud = pcd_obj_c.select_by_index(arm_ind)
-bg_cloud  = pcd_obj_c.select_by_index(arm_ind, invert=True)
-
-
-if remove_outliers:
-    del pcd_obj_c,pcd_obj_p
-    cl, ind        = arm_cloud.remove_statistical_outlier(nb_neighbors=40,std_ratio=2.0)
-    arm_cloud_down = arm_cloud.select_by_index(ind)
-
-    cl, ind        = bg_cloud.remove_statistical_outlier(nb_neighbors=40,std_ratio=2.0)
-    bg_cloud_donw  = arm_cloud.select_by_index(ind)
-    
-
-if reconstruct:
-    print("Reconstructing the arm points.")
-    new_pcd = surface_reconstruct(arm_cloud)
-    
-
-if visualize:
-    print("visualizing the arm points.")
-    o3d.visualization.draw_geometries([arm_cloud])
-
-    print("visualizing the background points.")
-    o3d.visualization.draw_geometries([bg_cloud])
-
-o3d.io.write_point_cloud("arm_cloud.pcd", arm_cloud, write_ascii=False, compressed=False, print_progress=True)
-o3d.io.write_point_cloud("bg_cloud.pcd", bg_cloud, write_ascii=False, compressed=False, print_progress=True)
-
-if reconstruct:
-    print("Writing reconstructed arm points.")
-    o3d.io.write_point_cloud("rec_arm_cloud.pcd", new_pcd, write_ascii=False, compressed=False, print_progress=True)
-
+        log.info("Reconstructing the arm points.")
+        target_data.surface_reconstruct_arm_pcd(visualize=False, verbose=True, sampling_method="uniform", filter_it=0)
+        
+        log.info("Saving the arm PCD.")
+        target_data.write_rec_arm_to_pcd("rec_arm_cloud.pcd")
