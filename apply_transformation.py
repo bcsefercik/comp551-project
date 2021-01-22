95b9be878f3d4f3214e5d380741a5f6e0cf637bc
Onur Berk Tore <otore19@ku.edu.tr>
2021-01-22 12:53:25 +0300

    Final  modifications from lab computer

--- apply_transformation.py
+++ apply_transformation.py
@@ -1,33 +1,28 @@
 import os
 import pickle 
 import PyKDL
-import numpy
+import numpy as np
 from tf.transformations import quaternion_from_euler
 
 folder_name       = '_gitignore/Dataset/p1/full_light/robotNet/train_raw_pose_p2/'
-output_name       = '_gitignore/Dataset/p1/full_light/robotNet/train/'
+output_name       = '_gitignore/Dataset/p1/full_light/robotNet/PLZ/'
 file_names        = [name for name in os.listdir(folder_name) if os.path.isdir(folder_name)]
+#robot_to_kinect   = [0.524, 0.31, 0.936, 0.33650, 0.358586, -0.603734, 0.627441]
+robot_to_kinect   = [0.524, 0.31, 0.936, -0.023, 0.963, -0.268, 0.001]
 
-robot_to_kinect   = [0.524, 0.31, 0.936, 0.33650, 0.358586, -0.603734, 0.627441]
-
-robot_to_kinect_translation = PyKDL.Vector(robot_to_kinect[0], robot_to_kinect[1], robot_to_kinect[2]) 
-<<<<<<< HEAD
-robot_to_kinect_rotation    = PyKDL.Rotation.Quaternion(robot_to_kinect[3], robot_to_kinect[4], robot_to_kinect[5], robot_to_kinect[6])
-robot_to_kinect_frame       = PyKDL.Frame(robot_to_kinect_rotation,robot_to_kinect_translation)
-=======
-robot_to_kinect_rotation    = PyKDL.Vector(robot_to_kinect[3], robot_to_kinect[4], robot_to_kinect[5])
-robot_to_kinect_frame       = PyKDL.Frame(robot_to_kinect_translation, robot_to_kinect_rotation)
->>>>>>> 25af3f55a75b45dcc2515fcc9b77325883003864
+base_to_camera_translation = PyKDL.Vector(robot_to_kinect[0], robot_to_kinect[1], robot_to_kinect[2]) 
+base_to_camera_rotation    = PyKDL.Rotation.Quaternion(robot_to_kinect[3], robot_to_kinect[4], robot_to_kinect[5], robot_to_kinect[6])
+base_to_camera_frame       = PyKDL.Frame(base_to_camera_rotation,base_to_camera_translation)
 
 def apply_transform(ee_pose):
     '''
         ee_pose :  (translation in meters) x , y, z  (ortientation) x, y, z, w
     '''
-    ee_translation  = PyKDL.Vector(ee_pose[0], ee_pose[1], ee_pose[2])      
+    ee_translation  = PyKDL.Vector(ee_pose[0], ee_pose[1], ee_pose[2])
     ee_rotation     = PyKDL.Rotation.Quaternion(ee_pose[3], ee_pose[4], ee_pose[5], ee_pose[6])
     ee_frame        = PyKDL.Frame(ee_rotation, ee_translation)
 
-    ee_kinect             = ee_frame * robot_to_kinect_frame.Inverse()
+    ee_kinect             = base_to_camera_frame.Inverse() * ee_frame
     ee_kinect_rotation    = ee_kinect.M
     ee_kinect_translation = ee_kinect.p
     r, p, y               = ee_kinect_rotation.GetRPY()
@@ -37,11 +32,14 @@ def apply_transform(ee_pose):
 
 i = 1
 for file_name in file_names:
-    print(i,'/' ,len(file_names))
+    print(file_name,'/' ,len(file_names))
     filehandler = open(folder_name + file_name,'rb')
     xyz_origin, rgb, label, instance_label,pose = pickle.load(filehandler)
+    print(pose)
+    #exit()
     filehandler.close()
     pose = apply_transform(pose)
+    print(file_name, pose)
     robotNetObject = (xyz_origin, rgb, label, instance_label,pose)
     filehandler = open(output_name+file_name,"wb")
     pickle.dump(robotNetObject, filehandler, protocol=2)
--- visualize_pcl_ee.py
+++ visualize_pcl_ee.py
@@ -19,7 +19,7 @@ import sklearn.preprocessing as preprocessing
 import pickle
 from os import listdir
 from os.path import isfile,isdir, join
-import trimesh
+#import trimesh
 
 from numpy.random import default_rng
 
@@ -31,15 +31,17 @@ def visualize_res(arm_xyz, arm_rgb, ee_position, ee_orientation,  visualize=True
         arm_pcd        = o3d.geometry.PointCloud()
         arm_pcd.points = o3d.utility.Vector3dVector(arm_xyz)
         arm_pcd.colors = o3d.utility.Vector3dVector(arm_rgb)
+
+
         
-        frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
+        frame    = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
         ee_frame = copy.deepcopy(frame).translate(ee_position)
         ee_frame.rotate(frame.get_rotation_matrix_from_quaternion(ee_orientation))
 
         o3d.visualization.draw_geometries([arm_pcd, ee_frame])
 
 
-arm_file_path = '/home/fnegahbani20/workplace/COMP551/comp551-project/_gitignore/pickle_samples/moving_67.pickle'
+arm_file_path = '/home/onurberk/Desktop/development/comp551-project/_gitignore/Dataset/p1/full_light/robotNet/moving_977.pickle'
 
 
 filehandler = open(arm_file_path, 'rb')
@@ -55,3 +57,4 @@ rgb[:,1]       = preprocessing.minmax_scale(rgb[:,1], feature_range = (0,1), axi
 rgb[:,2]       = preprocessing.minmax_scale(rgb[:,2], feature_range = (0,1), axis = 0)
 
 visualize_res(arm_points, rgb, (pose[0], pose[1], pose[2]), (pose[3], pose[4], pose[5], pose[6]))
+#0.50330381, -0.27686713,  0.00213259,  0.35378082,  0.82501708,0.26996977,  0.3482847
