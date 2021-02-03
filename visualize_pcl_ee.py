5866018c6b617a2dcf23053265e86d9bd599b42c
Bugra Can Sefercik <bsefercik@gmail.com>
2021-02-03 09:39:49 +0300

    pcl visualization done

--- visualize_pcl_ee.py
+++ visualize_pcl_ee.py
@@ -1,54 +1,39 @@
-import numpy as np
-import pickle as pkl
 import copy
-import logging as log
-import sklearn.preprocessing as preprocessing
 import pickle
-from os import listdir
-from os.path import isfile,isdir, join
-
 import os
 import argparse
 
 import open3d as o3d
-import ipdb
+import sklearn.preprocessing as preprocessing
 
 
-def visualize_res(arm_xyz, arm_rgb, ee_position, ee_orientation,  visualize=True, save=False):
-        '''
-            This function visualizes the pcd and ee pose or saves them.
-        '''
-        arm_pcd        = o3d.geometry.PointCloud()
-        arm_pcd.points = o3d.utility.Vector3dVector(arm_xyz)
-        arm_pcd.colors = o3d.utility.Vector3dVector(arm_rgb)
+def visualize_res(arm_xyz, arm_rgb, ee_position, ee_orientation, save=False):
+    '''
+        This function visualizes the pcd and ee pose or saves them.
+    '''
+    arm_pcd = o3d.geometry.PointCloud()
+    arm_pcd.points = o3d.utility.Vector3dVector(arm_xyz)
+    arm_pcd.colors = o3d.utility.Vector3dVector(arm_rgb)
+
+    frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
+    ee_frame = copy.deepcopy(frame).translate(ee_position)
+    ee_frame.rotate(frame.get_rotation_matrix_from_quaternion(ee_orientation))
+
+    o3d.visualization.draw_geometries([arm_pcd, ee_frame])
 
 
+def load_data(arm_file_path):
+    with open(arm_file_path, 'rb') as filehandler:
+        xyz_origin, rgb, label, instance_label, pose = pickle.load(filehandler, encoding='bytes')
 
-        frame    = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.25)
-        ee_frame = copy.deepcopy(frame).translate(ee_position)
-        ee_frame.rotate(frame.get_rotation_matrix_from_quaternion(ee_orientation))
+    arm_points = xyz_origin[label == 1]
 
-        o3d.visualization.draw_geometries([arm_pcd, ee_frame])
-        o3d.visualization.draw_geometries([arm_pcd, ee_frame])
+    rgb = rgb[label == 1]
+    rgb[:, 0] = preprocessing.minmax_scale(rgb[:, 0], feature_range=(0, 1), axis=0)
+    rgb[:, 1] = preprocessing.minmax_scale(rgb[:, 1], feature_range=(0, 1), axis=0)
+    rgb[:, 2] = preprocessing.minmax_scale(rgb[:, 2], feature_range=(0, 1), axis=0)
 
-
-arm_file_path = '/home/bcs/Desktop/MSc/repos/comp551_project/dataset/toy/train/moving_8.pickle'
-
-
-filehandler = open(arm_file_path, 'rb')
-
-xyz_origin, rgb, label, instance_label, pose = pickle.load(filehandler, encoding='bytes')
-
-filehandler.close()
-
-arm_points     = xyz_origin[label==1]
-rgb            = rgb[label==1]
-rgb[:,0]       = preprocessing.minmax_scale(rgb[:,0], feature_range = (0,1), axis = 0)
-rgb[:,1]       = preprocessing.minmax_scale(rgb[:,1], feature_range = (0,1), axis = 0)
-rgb[:,2]       = preprocessing.minmax_scale(rgb[:,2], feature_range = (0,1), axis = 0)
-
-visualize_res(arm_points, rgb, (pose[0], pose[1], pose[2]), (pose[3], pose[4], pose[5], pose[6]))
-#0.50330381, -0.27686713,  0.00213259,  0.35378082,  0.82501708,0.26996977,  0.3482847
+    return arm_points, rgb, pose
 
 
 if __name__ == "__main__":
@@ -59,8 +44,34 @@ if __name__ == "__main__":
 
     if os.path.isdir(args.pcl):
         file_names = os.listdir(args.pcl)
-        files = [os.path.join(args.pcl, fn) for fn in file_names if fn[-7:] == ".picke"]
+        files = [os.path.join(args.pcl, fn) for fn in file_names if fn[-7:] == ".pickle"]
     else:
         files = [args.pcl]
 
-    ipdb.set_trace()
+    i = 0
+    file_count = len(files)
+
+    while i < file_count:
+        filename = files[i].split('/')[-1]
+        arm_points, rgb, pose = load_data(files[i])
+
+        visualize_res(
+            arm_points,
+            rgb,
+            (pose[0], pose[1], pose[2]),
+            (pose[3], pose[4], pose[5], pose[6])
+        )
+
+        command = input(f"({filename}) $: ")
+
+        # Parse file commands and apply
+        if command == "rm":
+            os.remove(files[i])
+            del files[i]
+            file_count = len(files)
+
+            print(f"Removed {filename}.")
+
+            i -= 1
+
+        i += 1
