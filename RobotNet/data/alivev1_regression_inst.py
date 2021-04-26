18808aec263e743b537a3e80e5116cac67d57e41
Bugra Sefercik <bsefercik@gmail.com>
2021-04-27 00:33:04 +0300

    regression instance enhancements

--- RobotNet/data/alivev1_regression_inst.py
+++ RobotNet/data/alivev1_regression_inst.py
@@ -6,7 +6,7 @@ import torch
 import pickle
 from torch.utils.data import DataLoader
 
-sys.path.append('../')
+sys.path.append('../')  # noqa
 
 from util.config import cfg
 from util.log import logger
@@ -50,24 +50,10 @@ class Dataset:
                                 )
 
     def valLoader(self):
-        self.file_names['val'] = glob.glob(os.path.join(self.data_root, self.dataset, 'val', '*' + self.filename_suffix))
-        self.file_names['val'] = [fn for fn in self.file_names['val'] if fn[-16::] == '_semantic.pickle' and 'dark' not in fn]
-        self.file_names['val'].sort()
-        self.file_cnt   =  len(self.file_names['val'])
-        self.batch_cnt  = math.ceil(self.file_cnt / self.batch_size)
-        val_set = list(range(len(self.file_names['val'])))
-        self.val_data_loader = DataLoader(val_set, batch_size=self.batch_size, collate_fn=self.valMerge, num_workers=self.val_workers,
-                                          shuffle=False, drop_last=False, pin_memory=True)
+        self.val_data_loader = self.get_loader('val', self.valMerge, num_workers=self.val_workers, batch_size=self.batch_size)
 
     def testLoader(self):
-        self.file_names[self.test_split] = glob.glob(os.path.join(self.data_root, self.dataset, self.test_split, '*' + self.filename_suffix))
-        self.file_names[self.test_split] = [fn for fn in self.file_names[self.test_split] if fn[-16::] == '_semantic.pickle' and 'dark' not in fn]
-        self.file_names[self.test_split].sort()
-        self.file_cnt  =  len(self.file_names[self.test_split])
-        self.batch_cnt = math.ceil(self.file_cnt / self.batch_size)
-        test_set = list(np.arange(len(self.file_names[self.test_split])))
-        self.test_data_loader = DataLoader(test_set, batch_size=1, collate_fn=self.testMerge, num_workers=self.test_workers,
-                                           shuffle=False, drop_last=False, pin_memory=True)
+        self.test_data_loader = self.get_loader(self.test_split, self.testMerge, num_workers=self.test_workers)
 
     def get_loader(self, kw, collate_fn,
                    num_workers=1, batch_size=1, shuffle=False, drop_last=False):
@@ -136,7 +122,6 @@ class Dataset:
 
         return instance_num, {"instance_info": instance_info, "instance_pointnum": instance_pointnum}
 
-
     def dataAugment(self, xyz, jitter=False, flip=False, rot=False):
         m = np.eye(3)
         if jitter:
@@ -147,7 +132,6 @@ class Dataset:
             theta = np.random.rand() * 2 * math.pi
             m = np.matmul(m, [[math.cos(theta), math.sin(theta), 0], [-math.sin(theta), math.cos(theta), 0], [0, 0, 1]])  # rotation
         return np.matmul(xyz, m)
-
 
     def crop(self, xyz):
         '''
@@ -166,7 +150,6 @@ class Dataset:
             full_scale[:2] -= 32
 
         return xyz_offset, valid_idxs
-
 
     def getCroppedInstLabel(self, instance_label, valid_idxs):
         instance_label = instance_label[valid_idxs]
@@ -188,7 +171,7 @@ class Dataset:
 
         return x, original_data, curr_file_name
 
-    def trainMerge(self, id):
+    def merge(self, kw, id):
         semantics_scores = list()
         file_names = list()
         poses = list()
@@ -200,7 +183,7 @@ class Dataset:
         self.epoch = math.ceil(self.iteration_cnt/self.batch_cnt)
 
         for i, idx in enumerate(id):
-            semantics, (xyz_origin, rgb, label, instance_label, pose), file_name = self.get_data(idx, 'train')
+            semantics, (xyz_origin, rgb, label, instance_label, pose), file_name = self.get_data(idx, kw)
 
             xyz_middle = self.dataAugment(xyz_origin)
             xyz = xyz_middle
@@ -220,7 +203,7 @@ class Dataset:
             batch_offsets.append(batch_offsets[-1] + semantics.shape[0])
             file_names.append(file_name)
             poses.append(torch.from_numpy(np.array(pose, dtype=np.float32)))
-            locs.append(torch.cat([torch.LongTensor(xyz.shape[0], 1).fill_(i), torch.from_numpy(xyz).long()], 1)) #Cok garip birsey
+            locs.append(torch.cat([torch.LongTensor(xyz.shape[0], 1).fill_(i), torch.from_numpy(xyz).long()], 1))
             locs_float.append(torch.from_numpy(xyz_middle))
 
         # merge all the scenes in the batchd
@@ -240,247 +223,11 @@ class Dataset:
             'file_names': file_names
         }
 
+    def trainMerge(self, id):
+        return self.merge('train', id)
 
     def valMerge(self, id):
-        locs              = []
-        locs_float        = []
-        feats             = []
-        labels            = []
-        instance_labels   = []
-        poses             = []
-        file_names        = []
-        batch_offsets     = [0]
-        total_inst_num    = 0
-        instance_infos    = []  # (N, 9)
-        instance_pointnum = []  # (total_nInst), int
-
-        augment             = self.epoch  < self.prepare_epochs
-
-
-        for i, idx in enumerate(id):
-            (xyz_origin, rgb, label, instance_label, pose),file_name = self.get_data(idx,'val')
-            pose = np.array(pose)
-            ### flip x / rotation
-            xyz_middle = self.dataAugment(xyz_origin, augment, augment, augment)
-
-            ### scale
-            xyz = xyz_middle * self.scale
-
-            ### offset
-            xyz -= xyz.min(0)
-
-            ### crop
-            xyz, valid_idxs = self.crop(xyz)
-
-            xyz_middle = xyz_middle[valid_idxs]
-            xyz = xyz[valid_idxs]
-            rgb = rgb[valid_idxs]
-            label = label[valid_idxs]
-            instance_label = self.getCroppedInstLabel(instance_label, valid_idxs)
-
-            ### get instance information
-            inst_num, inst_infos = self.getInstanceInfo(xyz_middle, instance_label.astype(np.int32))
-            inst_info            = inst_infos["instance_info"]  # (n, 9), (cx, cy, cz, minx, miny, minz, maxx, maxy, maxz)
-            inst_pointnum        = inst_infos["instance_pointnum"]  # (nInst), list
-
-            instance_label[np.where(instance_label != -100)] += total_inst_num
-            total_inst_num += inst_num
-
-            ### merge the scene to the batch
-            batch_offsets.append(batch_offsets[-1] + xyz.shape[0])
-
-            locs.append(torch.cat([torch.LongTensor(xyz.shape[0], 1).fill_(i), torch.from_numpy(xyz).long()], 1))
-            locs_float.append(torch.from_numpy(xyz_middle))
-            feats.append(torch.from_numpy(rgb))
-            poses.append(torch.from_numpy(np.array(pose, dtype=np.float32)))
-            labels.append(torch.from_numpy(label))
-            file_names.append(file_name)
-            instance_labels.append(torch.from_numpy(instance_label))
-
-            instance_infos.append(torch.from_numpy(inst_info))
-            instance_pointnum.extend(inst_pointnum)
-
-        ### merge all the scenes in the batch
-        batch_offsets = torch.tensor(batch_offsets, dtype=torch.int)  # int (B+1)
-
-        locs = torch.cat(locs, 0)                                  # long (N, 1 + 3), the batch item idx is put in locs[:, 0]
-        locs_float = torch.cat(locs_float, 0).to(torch.float32)    # float (N, 3)
-        feats = torch.cat(feats, 0)                                # float (N, C)
-        poses           = torch.cat(poses,0)
-        labels = torch.cat(labels, 0).long()                       # long (N)
-        instance_labels = torch.cat(instance_labels, 0).long()     # long (N)
-
-        instance_infos = torch.cat(instance_infos, 0).to(torch.float32)               # float (N, 9) (meanxyz, minxyz, maxxyz)
-        instance_pointnum = torch.tensor(instance_pointnum, dtype=torch.int)          # int (total_nInst)
-
-        spatial_shape = np.clip((locs.max(0)[0][1:] + 1).numpy(), self.full_scale[0], None)  # long (3)
-
-        ### voxelize
-        voxel_locs, p2v_map, v2p_map = pointgroup_ops.voxelization_idx(locs, self.batch_size, self.mode)
-
-        return {'locs': locs, 'voxel_locs': voxel_locs, 'p2v_map': p2v_map, 'v2p_map': v2p_map,
-                'locs_float': locs_float, 'feats': feats, 'labels': labels, 'instance_labels': instance_labels,
-                'instance_info': instance_infos, 'instance_pointnum': instance_pointnum,
-                'id': id, 'offsets': batch_offsets, 'spatial_shape': spatial_shape, 'poses': poses,'file_names': file_names}
-
+        return self.merge('val', id)
 
     def testMerge(self, id):
-        locs            = []
-        locs_float      = []
-        feats           = []
-        poses           = []
-        labels          = []
-        instance_labels = []
-        file_names      = []
-        instance_infos    = []  # (N, 9)
-        instance_pointnum = []  # (total_nInst), int
-        batch_offsets     = [0]
-        total_inst_num    = 0
-
-        self.iteration_cnt += 1
-        self.epoch          = math.ceil(self.iteration_cnt/self.batch_cnt)
-        augment             = self.epoch  < self.prepare_epochs
-        self.scale          = self.scale if augment else 1
-
-        for i, idx in enumerate(id):
-            (xyz_origin, rgb, label, instance_label,pose),file_name = self.get_data(idx,'test')
-            pose = np.array(pose)
-            #print('Batch Scene No: ', i, 'Size is: ', xyz_origin.shape)
-
-            ### jitter / flip x / rotation
-            xyz_middle = self.dataAugment(xyz_origin, augment, augment, augment)
-            ### scale
-            xyz        = xyz_middle * self.scale
-
-            #xyz is xyz_midde scaled
-
-
-            ### elastic (No elastic distortion for regression)
-            if augment:
-                xyz = self.elastic(xyz, 6 * self.scale // 50, 40 * self.scale / 50)
-                xyz = self.elastic(xyz, 20 * self.scale // 50, 160 * self.scale / 50)
-
-            ### offset
-            xyz -= xyz.min(0)
-
-            ### crop
-            xyz, valid_idxs = self.crop(xyz)
-
-            xyz_middle     = xyz_middle[valid_idxs]
-            xyz            = xyz[valid_idxs]
-            rgb            = rgb[valid_idxs]
-            label          = label[valid_idxs]
-            instance_label = self.getCroppedInstLabel(instance_label, valid_idxs)
-
-            ### get instance information
-            inst_num, inst_infos = self.getInstanceInfo(xyz_middle, instance_label.astype(np.int32))
-            inst_info            = inst_infos["instance_info"]  # (n, 9), (cx, cy, cz, minx, miny, minz, maxx, maxy, maxz)
-            inst_pointnum        = inst_infos["instance_pointnum"]   # (nInst), list
-
-            instance_label[np.where(instance_label != -100)] += total_inst_num
-            total_inst_num                                   += inst_num
-
-            ### merge the scene to the batch
-            batch_offsets.append(batch_offsets[-1] + xyz.shape[0])
-
-            locs.append(torch.cat([torch.LongTensor(xyz.shape[0], 1).fill_(i), torch.from_numpy(xyz).long()], 1)) #Cok garip birsey
-            locs_float.append(torch.from_numpy(xyz_middle))
-            feats.append(torch.from_numpy(rgb) + torch.randn(3) * 0.1)
-            poses.append(torch.from_numpy(np.array(pose, dtype=np.float32)))
-            labels.append(torch.from_numpy(label))
-            file_names.append(file_name)
-            instance_labels.append(torch.from_numpy(instance_label))
-
-            instance_infos.append(torch.from_numpy(inst_info))
-            instance_pointnum.extend(inst_pointnum)
-
-        ### merge all the scenes in the batchd
-        batch_offsets = torch.tensor(batch_offsets, dtype=torch.int)  # int (B+1)
-
-        locs        = torch.cat(locs, 0)                                # long (N, 1 + 3), the batch item idx is put in locs[:, 0]
-        #locs_float = torch.cat(locs_float, 0).to(torch.float64) # float (N, 3)
-        locs_float  = torch.cat(locs_float, 0).to(torch.float32)  # float (N, 3)
-
-        feats           = torch.cat(feats, 0)                              # float (N, C)
-        poses           = torch.cat(poses,0)
-        labels          = torch.cat(labels, 0).long()                     # long (N)
-        instance_labels = torch.cat(instance_labels, 0).long()   # long (N)
-
-        instance_infos    = torch.cat(instance_infos, 0).to(torch.float32)       # float (N, 9) (meanxyz, minxyz, maxxyz)
-        instance_pointnum = torch.tensor(instance_pointnum, dtype=torch.int)  # int (total_nInst)
-
-        spatial_shape = np.clip((locs.max(0)[0][1:] + 1).numpy(), self.full_scale[0], None)     # long (3)
-
-        ### voxelize
-        voxel_locs, p2v_map, v2p_map = pointgroup_ops.voxelization_idx(locs, self.batch_size, self.mode) #This is not giving us sequential voxels in point space...
-        #p2v_map: N points -> M voxels (Use it with p2v_map.cpu().numpy(), its the combination of #batch_size scenes)
-        #v2p_map: M_voxels -> N points (Use it with v2p_map.cpu().numpy(), its the combination of #batch_size scenes)
-
-
-        return {'locs': locs, 'voxel_locs': voxel_locs, 'p2v_map': p2v_map, 'v2p_map': v2p_map,
-                'locs_float': locs_float, 'feats': feats, 'labels': labels, 'instance_labels': instance_labels,
-                'instance_info': instance_infos, 'instance_pointnum': instance_pointnum,
-                'id': id, 'offsets': batch_offsets, 'spatial_shape': spatial_shape, 'poses': poses,'file_names':file_names}
-
-        # locs          = []
-        # locs_float    = []
-        # feats         = []
-        # batch_offsets = [0]
-        # poses         = []
-
-        # augment       = self.epoch  < self.prepare_epochs
-
-        # for i, idx in enumerate(id):
-        #     if self.test_split == 'val':
-        #         (xyz_origin, rgb, label, instance_label, pose),file_name = self.get_data(idx,'val')
-        #         pose = np.array(pose)
-        #     elif self.test_split == 'test':
-        #         xyz_origin, rgb, _,__ = self.get_data(idx,'test')
-        #     else:
-        #         print("Wrong test split: {}!".format(self.test_split))
-        #         exit(0)
-
-        #     ### flip x / rotation
-        #     xyz_middle = self.dataAugment(xyz_origin, augment, augment, augment)
-
-        #     ### scale
-        #     xyz = xyz_middle * self.scale
-
-        #     ### offset
-        #     xyz -= xyz.min(0)
-
-        #     ### merge the scene to the batch
-        #     batch_offsets.append(batch_offsets[-1] + xyz.shape[0])
-
-        #     locs.append(torch.cat([torch.LongTensor(xyz.shape[0], 1).fill_(i), torch.from_numpy(xyz).long()], 1))
-        #     locs_float.append(torch.from_numpy(xyz_middle))
-        #     feats.append(torch.from_numpy(rgb))
-        #     poses.append(torch.from_numpy(pose))
-
-        # ### merge all the scenes in the batch
-        # batch_offsets = torch.tensor(batch_offsets, dtype=torch.int)  # int (B+1)
-
-        # locs = torch.cat(locs, 0)                                         # long (N, 1 + 3), the batch item idx is put in locs[:, 0]
-        # locs_float = torch.cat(locs_float, 0).to(torch.float32)           # float (N, 3)
-        # feats = torch.cat(feats, 0)                                       # float (N, C)
-        # poses           = torch.cat(poses,0)
-        # spatial_shape = np.clip((locs.max(0)[0][1:] + 1).numpy(), self.full_scale[0], None)  # long (3)
-
-        # ### voxelize
-        # voxel_locs, p2v_map, v2p_map = pointgroup_ops.voxelization_idx(locs, self.batch_size, self.mode)
-
-        # return {'locs': locs, 'voxel_locs': voxel_locs, 'p2v_map': p2v_map, 'v2p_map': v2p_map,
-        #         'locs_float': locs_float, 'feats': feats,
-        #         'id': id, 'offsets': batch_offsets, 'spatial_shape': spatial_shape, 'poses': poses}
-
-
-
-
-
-
-
-
-
-
-
-
+        return self.merge('test', id)
